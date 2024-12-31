a0e1a95221695cff681a503d12d62ed2e6665f98
msym7372 <images7372@outlook.com>
2025-01-01 00:01:41 +0900

    通信するタイミングを見直し

--- Scetch/Scetch.ino
+++ Scetch/Scetch.ino
@@ -1,63 +1,51 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
-
-const char* ssid = "hogefuga";
-const char* password = "hogepiyo";
-
-// 固定IP
-IPAddress local_IP(192, 168, 179, 10);
-IPAddress gateway(192, 168, 179, 1);
-IPAddress subnet(255, 255, 255, 0);
+#include "config.h"
 
 const int switchPin = 2;
-bool lastSwitchState = HIGH;
+static int previousSensorState = HIGH;
 
 unsigned long lastUpdated = 0; // 最後にデバウンス処理を行った時間
-unsigned long debounceDelay = 50;   // デバウンス間隔 (ミリ秒)
+unsigned long debounceDelay = 1000;   // デバウンス間隔 (ミリ秒)
 
-WebSocketsServer webSocket(81);          // WebSocketサーバーをポート81で作成
+WebSocketsServer webSocket(81);
 
 void setup() {
   Serial.begin(115200);
   pinMode(switchPin, INPUT_PULLUP);
 
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("Failed to configure static IP");
-  }
-
-  // Wi-Fi接続
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-
-  // WebSocketサーバー開始
+  initNetwork();
   webSocket.begin();
   Serial.println("WebSocket server started");
 }
 
 void loop() {
   webSocket.loop();
+  bool sensorState = digitalRead(switchPin);
+  // 状態が変わったときだけ処理
+  if(sensorState == LOW && previousSensorState == HIGH){
+      if(millis() - lastUpdated < debounceDelay){
+        return;
+      }
 
-  bool currentSwitchState = digitalRead(switchPin);
-  // Serial.println("switchState: " + switchState);
-
-  if (currentSwitchState != lastSwitchState) {  
-    if(millis() - lastUpdated < debounceDelay){
-      return;
-    }
-    lastUpdated = millis();
-
-    lastSwitchState = currentSwitchState; // 状態を更新
-
-
-    // スイッチが押された場合のみ通知
-    if (currentSwitchState == LOW) {
       Serial.println("PRESSED");
       webSocket.broadcastTXT("PRESSED");
-    }
+
+      lastUpdated = millis();
   }
+  previousSensorState = sensorState; // 状態を更新
+}
+
+void initNetwork() {
+  IPAddress local_IP(192, 168, 179, 10);
+  IPAddress gateway(192, 168, 179, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
+  WiFi.begin(CONF_SSID, CONF_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 }
 
